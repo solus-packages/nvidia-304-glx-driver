e2c43dd370f5ec1ef4d03a45af2565972ae655c5
Ikey Doherty <ikey@solus-project.com>
2015-12-15 08:52:44 +0000

    Enable multilib
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- actions.py
+++ actions.py
@@ -6,13 +6,13 @@ NoStrip = ["/"]
 from pisi.actionsapi import get, shelltools, pisitools, autotools, kerneltools
 import commands
 
-wdir = "NVIDIA-Linux-x86_64-%s-no-compat32" % get.srcVERSION()
+wdir = "NVIDIA-Linux-x86_64-%s" % get.srcVERSION()
 
 # Required... built in tandem with kernel update
 kversion = "4.1.14"
 
 def setup():
-    shelltools.system("sh NVIDIA-Linux-x86_64-%s-no-compat32.run --extract-only" % get.srcVERSION())
+    shelltools.system("sh NVIDIA-Linux-x86_64-%s.run --extract-only" % get.srcVERSION())
     shelltools.cd(wdir)
     shelltools.system("patch -p0 < ../nv-drm.patch")
 
@@ -20,16 +20,16 @@ def build():
     shelltools.cd(wdir + "/kernel")
     autotools.make("\"SYSSRC=/lib64/modules/%s/build\" module" % kversion)
 
-def link_install(lib, libdir='/usr/lib', abi='1'):
+def link_install(lib, libdir='/usr/lib64', abi='1', cdir='.'):
     ''' Install a library with necessary links '''
-    pisitools.dolib("%s.so.%s" % (lib, get.srcVERSION()), libdir)
+    pisitools.dolib("%s/%s.so.%s" % (cdir, lib, get.srcVERSION()), libdir)
     pisitools.dosym("%s/%s.so.%s" % (libdir, lib, get.srcVERSION()), "%s/%s.so.%s" % (libdir, os.path.basename(lib), abi))
     pisitools.dosym("%s/%s.so.%s" % (libdir, lib, abi), "%s/%s.so" % (libdir, os.path.basename(lib)))
 
 def install():
     # driver portion
     shelltools.cd(wdir)
-    pisitools.dolib("nvidia_drv.so", "/usr/lib/xorg/modules/drivers")
+    pisitools.dolib("nvidia_drv.so", "/usr/lib64/xorg/modules/drivers")
 
     kdir = "/lib64/modules/%s/kernel/drivers/video" % kversion
 
@@ -37,7 +37,9 @@ def install():
     libs = ["libGL", "libglx"]
     for lib in libs:
         abi = '2' if lib == "libGLESv2" else "1"
-        link_install(lib, "/usr/lib/glx-provider/nvidia", abi)
+        link_install(lib, "/usr/lib64/glx-provider/nvidia", abi)
+        if lib != "libglx":
+            link_install(lib, "/usr/lib32/glx-provider/nvidia", abi, cdir='32')
 
     # mesa/EGL compat
 
@@ -50,19 +52,29 @@ def install():
         version = compat[lib]
         versionMajor = version.split(".")[0]
 
-        pisitools.dosym("/usr/lib/glx-provider/default/%s.so.%s" % (lib, version), "/usr/lib/glx-provider/nvidia/%s.so" % (lib))
-        pisitools.dosym("/usr/lib/glx-provider/default/%s.so.%s" % (lib, version), "/usr/lib/glx-provider/nvidia/%s.so.%s" % (lib, versionMajor))
-        pisitools.dosym("/usr/lib/glx-provider/default/%s.so.%s" % (lib, version), "/usr/lib/glx-provider/nvidia/%s.so.%s" % (lib, version))
+        for suffix in ["lib64", "lib32"]:
+            pisitools.dosym("/usr/%s/glx-provider/default/%s.so.%s" % (suffix, lib, version), "/usr/%s/glx-provider/nvidia/%s.so" % (suffix, lib))
+            pisitools.dosym("/usr/%s/glx-provider/default/%s.so.%s" % (suffix, lib, version), "/usr/%s/glx-provider/nvidia/%s.so.%s" % (suffix, lib, versionMajor))
+            pisitools.dosym("/usr/%s/glx-provider/default/%s.so.%s" % (suffix, lib, version), "/usr/%s/glx-provider/nvidia/%s.so.%s" % (suffix, lib, version))
 
     # non-conflict libraries
-    libs =  ["libnvidia-glcore", "libnvidia-cfg", "libnvidia-ml", "tls/libnvidia-tls", "libcuda", "libnvcuvid",
+    libs =  ["libnvidia-glcore", "libnvidia-ml", "libcuda",
              "libnvidia-compiler", "libnvidia-opencl"]
 
+    native_libs = ["libnvidia-cfg", "libnvcuvid"]
     for lib in libs:
+        link_install(lib)
+        link_install(lib, libdir='/usr/lib32', cdir='32')
+    for lib in native_libs:
         link_install(lib)
 
     # vdpau provider
-    link_install("libvdpau_nvidia", "/usr/lib/vdpau")
+    link_install("libvdpau_nvidia", "/usr/lib64/vdpau")
+    link_install("32/libvdpau_nvidia", "/usr/lib32/vdpau")
+
+    # TLS
+    link_install("tls/libnvidia-tls")
+    link_install("tls/libnvidia-tls", libdir='/usr/lib32', cdir='32')
 
     # binaries
     bins = ["nvidia-debugdump", "nvidia-xconfig", "nvidia-settings",
