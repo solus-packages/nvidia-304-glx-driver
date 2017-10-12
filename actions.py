f5ce902337155ad3bc0f0b856b40505fa04b2feb
Ikey Doherty <ikey@solus-project.com>
2017-10-12 23:27:04 +0100

    Rebuild for kernels, add missing udev file
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- actions.py
+++ actions.py
@@ -8,8 +8,8 @@ import commands
 
 # Required... built in tandem with kernel update
 kernel_trees = {
-    "linux-lts": "4.9.53-51.lts",
-    "linux-current": "4.13.5-24.current"
+    "linux-lts": "4.9.56-52.lts",
+    "linux-current": "4.13.6-25.current"
 }
 
 def patch_dir(kernel):
